ee4c63d162b1a21309f456ade0aa1f011af27761
danalaw <danalaw.dl@gmail.com>
2017-07-16 03:49:48 +0100

    Initial Commit.
    IMPORTANT: Used a local database so remember to put the htdocs files into MAMP or WAMP or similar.

---
+++ htdocs/php_twitter/app_tokens.php
@@ -0,0 +1,11 @@
+<?php
+
+// OAuth tokens for single-user programming
+// After you create an app you MUST copy 
+// the tokens and paste them into the following lines
+$consumer_key = 'QVAjeU8Idhn1bOyJypwe3KSov';
+$consumer_secret = '2FgF8n5cJVKFamMwPjpyNXSJoD9k1RMDM74uquMa4FFl9YQTHK';
+$user_token = '720998133203918848-KI11S4GHCukZXuaoIQ7YS68bPYZYob4';
+$user_secret = 'aTUPckqVipv63eaJqw9l2HaznbOL6scsS7STuWleUzKZ3';
+
+?>
